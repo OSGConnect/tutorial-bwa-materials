049e962c9680890152d1d732f6333218f9313d65
Rachel Lombardi <37351287+rachellombardi@users.noreply.github.com>
2022-09-01 12:14:11 -0500

    removed refrence to trimmed files

--- download_data.sh
+++ download_data.sh
@@ -3,5 +3,6 @@
 curl -L -o data/ref_genome/ecoli_rel606.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/017/985/GCA_000017985.1_ASM1798v1/GCA_000017985.1_ASM1798v1_genomic.fna.gz
 curl -L -o sub.tar.gz https://ndownloader.figshare.com/files/14418248
 tar xvf sub.tar.gz
-mv sub/ data/trimmed_fastq_small
+mv sub/ data/fastq
+#mv sub/ data/trimmed_fastq_small
 rm sub.tar.gz
