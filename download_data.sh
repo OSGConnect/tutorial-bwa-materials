6f8007a722d7f030560386d50282fe07f1291acb
Rachel Lombardi <37351287+rachellombardi@users.noreply.github.com>
2022-09-06 11:17:21 -0500

    Delete download_data.sh

--- download_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -L -o data/ref_genome/ecoli_rel606.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/017/985/GCA_000017985.1_ASM1798v1/GCA_000017985.1_ASM1798v1_genomic.fna.gz
-curl -L -o sub.tar.gz https://ndownloader.figshare.com/files/14418248
-tar xvf sub.tar.gz
-mv sub/ data/fastq
-#mv sub/ data/trimmed_fastq_small
-rm sub.tar.gz
